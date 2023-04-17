8b6a67d08467e3943885f300c1c19ac6014b012c
Davi Dubeux <davidubeux10@gmail.com>
2023-04-17 18:07:35 -0300

    teste1

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 
 int main() {
 
-  // teste
+  PwmOut potencia(PA_0);
+  DigitalOut sentido1(PA_1);
+  DigitalOut sentido2(PA_2);
+
 
   while(1) {
-    // put your main code here, to run repeatedly:
+    
+    potencia = 0.5;
+    sentido1 = 1;
+    sentido2 = 0;
+    
   }
 }
