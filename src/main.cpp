3dde6712d9966fba66c46cadb2658c98c292d16a
Davi Dubeux <davidubeux10@gmail.com>
2023-04-19 12:31:16 -0300

    teste2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
 #include <mbed.h>
 
+  DigitalOut modo(PA_0);  // Mode = 1 - PH/EN Control Mode
+  DigitalOut enable(PA_1); //Enable - ligado ou n
+  DigitalOut phase(PA_2); //Phase - horário ou anti-horário
+
+  PwmOut potencia(PB_0);
+
 int main() {
 
-  PwmOut potencia(PA_0);
-  DigitalOut sentido1(PA_1);
-  DigitalOut sentido2(PA_2);
+  modo = 1;
+  enable = 1;
+  phase = 0;
 
+  potencia = 0.5;
 
   while(1) {
     
-    potencia = 0.5;
-    sentido1 = 1;
-    sentido2 = 0;
-    
+    phase = 1;
+    ThisThread::sleep_for(1);
+    phase = 0;
+    ThisThread::sleep_for(1);
+
   }
 }
