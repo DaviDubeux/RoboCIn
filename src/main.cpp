4bc728459c62b261751759525c8dd64e131084db
Davi Dubeux <davidubeux10@gmail.com>
2023-04-25 20:59:10 -0300

    teste 4 readEncoder()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <mbed.h>
+#include <iostream>
 
   DigitalOut enable(PA_1); //Enable - PWM
   DigitalOut phase(PA_2); //Phase - horário ou anti-horário
+  DigitalIn ChA(PC_0);
+  DigitalIn ChB(PC_1);
+
+  int pos = 0;
+
+  void readEncoder() {
+    int b = ChA;
+    if (b>0) {
+      pos++;
+    }
+    else{
+      pos--;
+    }
+    }
+
 
 int main() {
 
@@ -9,14 +25,18 @@ int main() {
     
     enable = 0.4;
     phase = 1;
+    cout<<pos;
     ThisThread::sleep_for(1);
     phase = 0;
+    cout<<pos;
     ThisThread::sleep_for(1);
 
     enable = 0.8;
     phase = 1;
+    cout<<pos;
     ThisThread::sleep_for(1);
     phase = 0;
+    cout<<pos;
     ThisThread::sleep_for(1);
 
   }
