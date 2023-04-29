30031d2f5b47369a77c2cafb076b7f7adcf2d4f7
Davi Dubeux <davidubeux10@gmail.com>
2023-04-29 17:31:28 -0300

    filter PID target e drive

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,18 @@ Timer t;
 long prevT = 0;
 int prevPos = 0;
 volatile int pos_i = 0;
+float vFilt = 0;
+float prevV = 0;
+float eIntegral = 0;
+
+/*
+//Monitor serial ******
+static BufferredSerial serial_port(USBTX, USBRX, 115200);
+
+FileHandle *mbed::mbed_override_console(int fd) {
+  return &serial_port;
+}
+*/
 
 void readEncoder() {
   int b = ChB;
@@ -44,8 +56,47 @@ int main() {
    float velocity = (pos - prevPos)/deltaT;
    prevPos = pos;
    prevT = currT;
-
-   cout<<velocity<<endl;
    
+   //convert to rpm
+   float v = velocity/4096.0*60.0;
+   
+   /*
+  //Low-pass filter ***********
+  vFilt = a*vFilt + b*v + b*prevV;
+  prevV = v;
+  */
+
+  // Set a target
+  float vT = 100*(sin(currT/1e6)>0);
+
+  /*
+  //PID
+  float kp = 1;
+  float ki = 0;
+  float e = vT - v;
+  eIntegral = eIntegral + e*deltaT;
+
+  float u = kp*e + ki*eIntegral;
+  */
+
+  /*
+  //Set enable and phase
+  int dir = 1;
+  if (u<0){
+    dir = -1;
+  }
+  int pwr = (int) fabs(u);
+  if (pwr > 255){
+    pwr = 255;
+  }
+  phase = dir;
+  enable = pwr;
+  */
+
+
+  /*
+  cout<<vFilt<<endl;
+  ThisThread::sleep_for(1);
+  */
   }
 }
