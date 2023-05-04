e3af8b398b6ada9f2a5f807a7b6efac23f0d6143
Davi Dubeux <davidubeux10@gmail.com>
2023-05-04 20:58:38 -0300

    final

---
+++ src/README.md
@@ -0,0 +1,17 @@
+Os maiores problemas na execução do projeto foram definir
+
+A idéia da função readPos era comparar a posição dos canais A e B para assim ter também a medição do sentido de rotação do motor, já que minha idéia inicial apresentava problemas em altas velocidades no dia 04/05;
+
+a função original era:
+void readEncoder() {
+int b = ChB;
+pos = 0;
+if (b>0) {
+pos++;
+}
+else {
+pos--;
+}
+}
+
+entretando, a altas velocidades, a rotação do motor está tão alta que, no intervalo entre o interrupt chamar a voidEncoder e a função ler o canal b para identificar se a posição aumentou ou diminuiu, o valor do ChB já mudou, gerando vários errors
--- src/main.cpp
+++ src/main.cpp
@@ -7,80 +7,100 @@ using namespace std::chrono;
 PwmOut enable (PB_5);
 DigitalOut phase (PB_6);
 InterruptIn ChA(PA_8);
-DigitalIn ChB(PA_9);
+InterruptIn ChB(PA_9);
 
-//globals
-int pos = 0;
-float deltaPos = 0;
-float velocity = 0;
-
-float vFilt = 0;
-float prevV = 0;
-
-float eIntegral = 0;
-
-//Ticker
-Ticker timer;
-
-//Monitor serial ******
+//Monitor serial
 static BufferedSerial serial_port(USBTX, USBRX, 115200);
 FileHandle *mbed::mbed_override_console(int fd) {
   return &serial_port;
 }
 
-void readEncoder() {
-  int b = ChB;
-  if (b>0) {
-    pos++;
-  }
-  else {
-    pos--;
-  }
+//globals
+int pos = 0;
+int posA = 0;
+int posB = 0;
+float deltaPos = 0;
+float velocity = 0;
+int i = 100;
+float vFilt = 0;
+float prevV = 0;
+float eIntegral = 0;
+float ePrev = 0;
+
+//define uma RPM target
+float vT = 300.0;
+
+//Tickers 
+Ticker timer;
+Ticker pwm;
+
+void readPosA(){
+  posA++;
 }
+// void readPosB(){
+//   posB++;
+// }
+
+void readPos() {
+  pos = posA;
+  posA = 0;
+  // posB = 0;
+;
+  }
 
 void update(){
-  velocity = pos/10; //deltaT float
+  readPos();
+  velocity = pos/1; //1: deltaT float
   pos = 0;
 }
 
-int main() {
-  ChA.rise(&readEncoder);
-  timer.attach(&update, 10ms);
+void vTupdate(){
+  i = (-1)*i;
+  vT = 200 + i;
+}
+
+int main() {  
+  enable.period(1.0/1000);
+
+  ChA.rise(&readPosA);
+  // ChB.rise(&readPosB);
+  timer.attach(&update, 1ms); //deltaT chrono
+  pwm.attach(&vTupdate, 1000ms); 
 
   while(1) {
 
-    //convert to rpm
-    float v = velocity/4096.0*1000.0*60.0;
+  //converter para RPM
+  long double v = velocity/4096.0*1000.0*60.0/5.0;
 
-    //Low-pass filter
-    /*
-    vFilt = 1*vFilt + 2*v + 2*preV
-    prevV = v;
-    */
+  //constantes PID
+  float kp = 0.005;
+  float ki = 0.001;
 
-    // Set a target
-    float vT = 1500;
+  //define variáveis de erro
+  float e = vT - v;
+  eIntegral = eIntegral + e*0.001;
 
-    //PID
-    float kp = 0.001;
-    float ki = 0.01;
-    float e = vT - v;
-    eIntegral = eIntegral + e*0.01;
+  //calcula o ajuste (u)
+  float u = kp*e + ki*eIntegral;
 
-    float u = kp*e + ki*eIntegral;
+  //problemas devido a mudança de sentido do motor
+  // int dir = 0;
+  // if (u<0){
+  //   dir = 1;
+  // }
+  // int pwr = (int) fabs(u);
+  // if (pwr > 1){
+  //   pwr = 1;
+  // }
+  // phase = dir;
+  // enable = pwr;
 
-    //Set enable and phase
-    int dir = 1;
-    if (u<0){
-      dir = -1;
-    }
-    int pwr = (int) fabs(u);
-    if (pwr > 1){
-      pwr = 1;
-    }
-    phase = dir;
-    enable = pwr;
+  //define direção e sentido do motor
+  phase = 0;
+  enable = u;
 
-    printf("%f\n", v);
+  //printa as velocidades target e medidas no plotter
+  printf("%f ", vT);
+  printf("%Lf\n", v);
   }
 }
